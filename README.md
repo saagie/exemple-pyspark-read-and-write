cf8d7bd9718c27a5e662e2938e6ad4156f5bf7db
Erwan KOFFI <erwan@saagie.com>
2017-10-19 11:55:11 +0200

    More concise examples and usage instructions

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # exemple-pyspark-read-and-write
+
+To use this example as is, you can either start a PySpark Notebook
+on Data Fabric and import the notebook or zip the ```__main__.py``` file
+and create a regular PySpark Job.
---
+++ __main__.py
@@ -0,0 +1,14 @@
+# In order to work as is on Data Fabric, this file should be inside a zip archive.
+from pyspark.sql import SparkSession
+
+sparkSession = SparkSession.builder.appName("example-pyspark-read-and-write").getOrCreate()
+data = [('First', 1), ('Second', 2), ('Third', 3), ('Fourth', 4), ('Fifth', 5)]
+df = sparkSession.createDataFrame(data)
+
+
+# Write into HDFS
+df.write.csv("hdfs://cluster/user/hdfs/test/example.csv")
+
+# Read from HDFS
+df_load = sparkSession.read.csv('hdfs://cluster/user/hdfs/test/example.csv')
+df_load.show()
